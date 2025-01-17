04710a5e37aba8b139d1bd30138fd65b3b35b58f
Pratik Datir <139746305+PratikD24@users.noreply.github.com>
2025-01-17 10:44:26 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (32.67%) - LeetSync

--- 338-counting-bits/counting-bits.cpp
+++ 338-counting-bits/counting-bits.cpp
@@ -1,20 +1,39 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-        
+        //O(n) solution
         vector<int>ans(n+1,0);
+
         for(int i=0;i<=n;i++)
         {
-            int num = i;
-            int count = 0;
-
-            while(num)
+            if(i%2 == 0)
             {
-                count += num%2 ;
-                num = num/2;
+                ans[i] = ans[i/2];
             }
-            ans[i] = count;
+            else
+            {
+                ans[i]= ans[i/2]+1;
+            }
         }
         return ans;
+
+        //n*log(n)
+
+        // for(int i=0;i<=n;i++)
+        // {
+        //     int num = i;
+        //     int count = 0;
+
+        //     while(num)
+        //     {
+        //         count += num%2 ;
+        //         num = num/2;
+        //     }
+        //     ans[i] = count;
+        // }
+        // return ans;
+
+
+
     }
 };
