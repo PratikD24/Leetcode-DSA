5902c75a5c5f7983453cbdda3e6fc72e7692455b
Pratik Datir <139746305+PratikD24@users.noreply.github.com>
2025-04-10 19:56:38 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (32.95%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,44 +1,32 @@
 class Solution {
 public:
 
-    int rec(int i ,int j , vector<vector<int>> &dp){
+    int rec(int i , int j,int m,int n,vector<vector<int>> &dp)
+    {
+        if(i==m-1 && j==n-1)
+        {
+            return 1;
+        }
 
-        //base case
-        if(i==0 && j == 0) return 1;
-        if(i<0 || j<0) return 0;
+        if(i>=m || j>= n)
+        {
+            return 0;
+        }
 
-        if(dp[i][j] != -1) return dp[i][j]; // already visited
-
-        int up = rec(i-1,j,dp);
-        int left = rec(i,j-1,dp);
-
-        return dp[i][j] = up + left;
+        if(dp[i][j] != -1)
+        {
+            return dp[i][j];
+        }
+        else
+        {
+            return dp[i][j] = rec(i+1,j,m,n,dp) + rec(i,j+1,m,n,dp);
+        }
     }
-
 
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m,vector<int>(n,-1)); // -1 = Not visited
-
-        return rec(m-1,n-1,dp) % (2 * 1000000000);
+        
+        //base 
+        vector<vector<int>>dp(m,vector<int>(n,-1));
+        return rec(0,0,m,n,dp);
     }
 };
-
-
-// class Solution {
-// private:
-//     int f(int i, int j, vector<vector<int>> &dp){
-//         if(i == 0 && j == 0) return 1;
-//         if(i < 0 || j < 0) return 0;
-
-//         if(dp[i][j] != -1) return dp[i][j];
-
-//         int up = f(i-1, j, dp);
-//         int left = f(i, j-1, dp);
-//         return dp[i][j] = up + left;
-//     }
-// public:
-//     int uniquePaths(int m, int n) {
-//         vector<vector<int>> dp (m, vector<int>(n, -1));
-//         return f(m-1, n-1, dp);
-//     }
-// };
