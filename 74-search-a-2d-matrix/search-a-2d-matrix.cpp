b828f08213750ceb8faefdd9a7f325b17221e0c4
Pratik Datir <139746305+PratikD24@users.noreply.github.com>
2025-04-19 12:04:44 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (76.62%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -4,16 +4,29 @@ public:
         int n = matrix.size();
         int m = matrix[0].size();
 
-        for(int i = 0; i<n;i++ )
+        int low = 0;
+        int high = n*m - 1;
+
+        while(low <= high)
         {
-            for(int j=0;j<m;j++)
+            int mid = (high + low)/2;
+            int row = mid/m;
+            int col = mid % m;
+
+            if(matrix[row][col] == target)
             {
-                if(matrix[i][j] == target)
-                {
-                    return true;
-                }
+                return true;
+            }
+            else if(matrix[row][col] < target)
+            {
+                low = mid +1;
+            }
+            else
+            {
+                high =mid-1;
             }
         }
+
         return false;
     }
 };
